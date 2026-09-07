ddf4915651b168bad76a3286afbe17c516b93346
gfetmail-rgb <gfetmail@gmail.com>
2026-09-07 22:05:25 +0330

    Add identifier management buttons

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,54 @@ namespace Trade.It
         public Form1()
         {
             InitializeComponent();
+
+            var identifierButtonsPanel = new FlowLayoutPanel
+            {
+                Dock = DockStyle.Top,
+                Height = 48,
+                Padding = new Padding(4),
+                FlowDirection = FlowDirection.RightToLeft,
+                WrapContents = false,
+                RightToLeft = RightToLeft.Yes
+            };
+
+            identifierButtonsPanel.Controls.Add(new Button
+            {
+                Text = "جدید",
+                Width = 58,
+                Height = 34,
+                Margin = new Padding(3)
+            });
+            identifierButtonsPanel.Controls.Add(new Button
+            {
+                Text = "ذخیره",
+                Width = 65,
+                Height = 34,
+                Margin = new Padding(3)
+            });
+            identifierButtonsPanel.Controls.Add(new Button
+            {
+                Text = "حذف",
+                Width = 58,
+                Height = 34,
+                Margin = new Padding(3)
+            });
+            identifierButtonsPanel.Controls.Add(new Button
+            {
+                Text = "حذف همه",
+                Width = 78,
+                Height = 34,
+                Margin = new Padding(3)
+            });
+            identifierButtonsPanel.Controls.Add(new Button
+            {
+                Text = "ورود از اکسل",
+                Width = 105,
+                Height = 34,
+                Margin = new Padding(3)
+            });
+
+            tabPage3.Controls.Add(identifierButtonsPanel);
         }
 
         private void nameTextBox_TextChanged(object sender, EventArgs e)
