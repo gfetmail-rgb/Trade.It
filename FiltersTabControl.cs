7069cbfa50f536ccb85a2d6f2b22fdaf50f3b810
gfetmail-rgb <gfetmail@gmail.com>
2026-09-07 20:52:56 +0330

    Create designer-managed filters tab control

---
+++ FiltersTabControl.cs
@@ -0,0 +1,10 @@
+namespace Trade.It
+{
+    public partial class FiltersTabControl : UserControl
+    {
+        public FiltersTabControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
