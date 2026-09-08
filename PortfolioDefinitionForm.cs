25759e9da6659f121118722a692229095d95a551
gfetmail-rgb <gfetmail@gmail.com>
2026-09-08 18:31:51 +0330

    Add designer-backed portfolio definition form

---
+++ PortfolioDefinitionForm.cs
@@ -0,0 +1,10 @@
+namespace Trade.It
+{
+    public partial class PortfolioDefinitionForm : Form
+    {
+        public PortfolioDefinitionForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
