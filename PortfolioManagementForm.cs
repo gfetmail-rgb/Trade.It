8b5774acc87d1bf1b93d39bc79d3beba09097ded
gfetmail-rgb <gfetmail@gmail.com>
2026-09-08 19:34:04 +0330

    Fix Portfolio Management Designer initialization

--- PortfolioManagementForm.cs
+++ PortfolioManagementForm.cs
@@ -5,6 +5,16 @@ namespace Trade.It
         public PortfolioManagementForm()
         {
             InitializeComponent();
+            InitializeParameterRows();
+        }
+
+        private void InitializeParameterRows()
+        {
+            parameterTable.RowStyles.Clear();
+            for (int i = 0; i < 6; i++)
+            {
+                parameterTable.RowStyles.Add(new RowStyle(SizeType.Percent, 16.66F));
+            }
         }
     }
 }
