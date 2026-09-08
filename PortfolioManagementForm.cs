96afdb117a8e10c1e180da63ce01079669c9a94f
gfetmail-rgb <gfetmail@gmail.com>
2026-09-08 20:03:28 +0330

    Move PortfolioManagementForm layout initialization into Designer

--- PortfolioManagementForm.cs
+++ PortfolioManagementForm.cs
@@ -5,16 +5,6 @@ namespace Trade.It
         public PortfolioManagementForm()
         {
             InitializeComponent();
-            InitializeParameterRows();
-        }
-
-        private void InitializeParameterRows()
-        {
-            parameterTable.RowStyles.Clear();
-            for (int i = 0; i < 6; i++)
-            {
-                parameterTable.RowStyles.Add(new RowStyle(SizeType.Percent, 16.66F));
-            }
         }
     }
 }
