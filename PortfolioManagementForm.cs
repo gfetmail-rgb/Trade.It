222f7626185001f08e7fdb24ef111f9e4c718fcf
gfetmail-rgb <gfetmail@gmail.com>
2026-09-08 18:32:22 +0330

    Add designer-backed portfolio management form

---
+++ PortfolioManagementForm.cs
@@ -0,0 +1,10 @@
+namespace Trade.It
+{
+    public partial class PortfolioManagementForm : Form
+    {
+        public PortfolioManagementForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
