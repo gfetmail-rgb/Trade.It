1ac12bd5f07637594b93f7c024f4da02074bce63
gfetmail-rgb <gfetmail@gmail.com>
2026-09-11 15:13:23 +0330

    Initialize drawing toolbar when MainForm is created by WinForms Designer

---
+++ MainForm.DrawingDesigner.cs
@@ -0,0 +1,11 @@
+namespace Trade.It
+{
+    public partial class MainForm
+    {
+        protected override void OnCreateControl()
+        {
+            base.OnCreateControl();
+            InitializeChartDrawingTools();
+        }
+    }
+}
