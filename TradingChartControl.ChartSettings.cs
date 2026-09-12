7934ac54e2afcd22afe8ffbd6c76f895863fcaff
gfetmail-rgb <gfetmail@gmail.com>
2026-09-12 19:21:01 +0330

    Use persistent chart appearance settings

--- TradingChartControl.ChartSettings.cs
+++ TradingChartControl.ChartSettings.cs
@@ -2,6 +2,13 @@ namespace Trade.It
 {
     internal sealed partial class TradingChartControl
     {
-        internal static double ChartRightEmptyPercent { get; set; } = 25.0;
+        internal static double ChartRightEmptyPercent
+        {
+            get => ChartAppearanceSettings.ChartRightEmptyPercent;
+            set => ChartAppearanceSettings.SetChartRightEmptyPercent(value);
+        }
+
+        internal static void LoadChartAppearanceSettings() => ChartAppearanceSettings.Load();
+        internal static void SaveChartAppearanceSettings() => ChartAppearanceSettings.Save();
     }
 }
