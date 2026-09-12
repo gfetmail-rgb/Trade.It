8779a6de422e671b5d4224c3be1a7eaa63cc175e
gfetmail-rgb <gfetmail@gmail.com>
2026-09-12 20:51:39 +0330

    Remove unused بازنشانی menu item

---
+++ MainForm.MenuCleanup.cs
@@ -0,0 +1,11 @@
+namespace Trade.It
+{
+    public partial class MainForm
+    {
+        protected override void OnCreateControl(EventArgs e)
+        {
+            base.OnCreateControl(e);
+            resetMenuItem.Visible = false;
+        }
+    }
+}
