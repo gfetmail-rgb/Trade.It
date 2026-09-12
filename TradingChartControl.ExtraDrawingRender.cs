eca19ba7ef3ad298e37afcf3feec8de9ee37b2ad
gfetmail-rgb <gfetmail@gmail.com>
2026-09-12 19:24:21 +0330

    Apply configurable extra drawing colors

--- TradingChartControl.ExtraDrawingRender.cs
+++ TradingChartControl.ExtraDrawingRender.cs
@@ -2,34 +2,38 @@ namespace Trade.It
 {
     internal sealed partial class TradingChartControl
     {
+        private static Color GetExtraDrawingColor(ExtraDrawingTool tool) => tool switch
+        {
+            ExtraDrawingTool.Pitchfork => ChartAppearanceSettings.PitchforkColor,
+            ExtraDrawingTool.FibonacciExtension => ChartAppearanceSettings.FibonacciExtensionColor,
+            ExtraDrawingTool.Measure => ChartAppearanceSettings.MeasureColor,
+            _ => ChartAppearanceSettings.PitchforkColor
+        };
+
         private void RenderExtraDrawings(Graphics g)
         {
             SyncExtraDrawingData();
             if (!TryGetExtraContext(out var plot, out var visibleCountForDrawing, out var min, out var max))
                 return;
 
-            using var normalPen = new Pen(Color.FromArgb(155, 80, 45), 1.3f);
-            using var selectedPen = new Pen(Color.FromArgb(190, 55, 35), 2.2f);
             using var labelBrush = new SolidBrush(Color.FromArgb(35, 35, 35));
-            using var previewPen = new Pen(Color.FromArgb(155, 80, 45), 1.2f)
-            {
-                DashStyle = System.Drawing.Drawing2D.DashStyle.Dash
-            };
 
             for (var i = 0; i < extraDrawings.Count; i++)
             {
                 var d = extraDrawings[i];
-                var isSelected = i == selectedExtraDrawingIndex;
-                var pen = isSelected ? selectedPen : normalPen;
+                var color = GetExtraDrawingColor(d.Tool);
+                using var pen = new Pen(color, i == selectedExtraDrawingIndex ? 2.2f : 1.3f);
 
                 if (d.Tool == ExtraDrawingTool.Pitchfork)
-                    DrawPitchfork(g, pen, d, plot, visibleCountForDrawing, min, max, isSelected);
+                    DrawPitchfork(g, pen, d, plot, visibleCountForDrawing, min, max, i == selectedExtraDrawingIndex);
                 else if (d.Tool == ExtraDrawingTool.FibonacciExtension)
-                    DrawThreePointFibonacci(g, pen, labelBrush, d, plot, visibleCountForDrawing, min, max, isSelected);
+                    DrawThreePointFibonacci(g, pen, labelBrush, d, plot, visibleCountForDrawing, min, max, i == selectedExtraDrawingIndex);
             }
 
             if (extraDrawingInProgress && ExtraDrawingActive && extraDrawingPoints.Count > 0)
             {
+                var previewColor = GetExtraDrawingColor(activeExtraDrawingTool);
+                using var previewPen = new Pen(previewColor, 1.2f) { DashStyle = System.Drawing.Drawing2D.DashStyle.Dash };
                 if (activeExtraDrawingTool == ExtraDrawingTool.Measure)
                     DrawMeasurePreview(g, previewPen, labelBrush, extraDrawingPoints[0], extraDrawingCurrentPoint, plot, visibleCountForDrawing, min, max);
                 else if (activeExtraDrawingTool == ExtraDrawingTool.Pitchfork)
@@ -46,7 +50,6 @@ namespace Trade.It
             var c = DataToScreen(d.X3, d.Y3, plot, visibleCount, min, max);
             var dy = b.Y - a.Y;
             var levels = new[] { 0f, 0.382f, 0.618f, 1f, 1.272f, 1.618f, 2f, 2.618f };
-
             var leftX = Math.Min(a.X, c.X);
             var rightX = Math.Max(a.X, c.X);
 
@@ -54,7 +57,6 @@ namespace Trade.It
             {
                 var y = c.Y + dy * level;
                 g.DrawLine(pen, leftX, y, rightX, y);
-
                 var text = level switch
                 {
                     0f => "0%",
@@ -66,19 +68,17 @@ namespace Trade.It
                     2f => "200%",
                     _ => "261.8%"
                 };
-
                 var size = g.MeasureString(text, SystemFonts.DefaultFont);
                 var labelX = rightX + 5f;
-                if (labelX + size.Width > plot.Right)
-                    labelX = Math.Max(plot.Left, leftX - size.Width - 5f);
+                if (labelX + size.Width > plot.Right) labelX = Math.Max(plot.Left, leftX - size.Width - 5f);
                 g.DrawString(text, SystemFonts.DefaultFont, labelBrush, labelX, y - size.Height / 2f);
             }
 
             if (selected)
             {
-                DrawAdvancedHandle(g, a);
-                DrawAdvancedHandle(g, b);
-                DrawAdvancedHandle(g, c);
+                DrawAdvancedHandle(g, a, pen.Color);
+                DrawAdvancedHandle(g, b, pen.Color);
+                DrawAdvancedHandle(g, c, pen.Color);
             }
         }
     }
