d2efa01002cefec14d5f423522f0182b0eedbd8a
gfetmail-rgb <gfetmail@gmail.com>
2026-09-12 19:22:22 +0330

    Expose configured drawing colors to chart rendering

--- TradingChartControl.ChartSettings.cs
+++ TradingChartControl.ChartSettings.cs
@@ -10,5 +10,26 @@ namespace Trade.It
 
         internal static void LoadChartAppearanceSettings() => ChartAppearanceSettings.Load();
         internal static void SaveChartAppearanceSettings() => ChartAppearanceSettings.Save();
+
+        private static Color GetDrawingColor(ChartDrawingTool tool) => tool switch
+        {
+            ChartDrawingTool.TrendLine => ChartAppearanceSettings.TrendLineColor,
+            ChartDrawingTool.TrendChannel => ChartAppearanceSettings.TrendChannelColor,
+            ChartDrawingTool.HorizontalDoubleArrow => ChartAppearanceSettings.HorizontalDoubleArrowColor,
+            ChartDrawingTool.VerticalDoubleArrow => ChartAppearanceSettings.VerticalDoubleArrowColor,
+            ChartDrawingTool.HorizontalRay => ChartAppearanceSettings.HorizontalRayColor,
+            ChartDrawingTool.TrendLineWithArrow => ChartAppearanceSettings.TrendLineWithArrowColor,
+            ChartDrawingTool.Rectangle => ChartAppearanceSettings.RectangleColor,
+            _ => ChartAppearanceSettings.TrendLineColor
+        };
+
+        private static Color DarkenColor(Color color, float factor = 0.72f)
+        {
+            return Color.FromArgb(
+                color.A,
+                (int)(color.R * factor),
+                (int)(color.G * factor),
+                (int)(color.B * factor));
+        }
     }
 }
