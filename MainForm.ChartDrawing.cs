e3bfd4a4cd62f9c62ab59b31f0e9fab925726aa5
gfetmail-rgb <gfetmail@gmail.com>
2026-09-12 23:49:55 +0330

    Reset extra drawing mouse safety when switching tools

--- MainForm.ChartDrawing.cs
+++ MainForm.ChartDrawing.cs
@@ -62,6 +62,7 @@ namespace Trade.It
 
             chart.CancelAdvancedDrawing();
             chart.CancelExtraDrawing();
+            chart.EnableExtraDrawingMouseSafety();
 
             if (chart.ActiveDrawingTool == tool)
             {
@@ -84,6 +85,7 @@ namespace Trade.It
 
             chart.CancelDrawing();
             chart.CancelExtraDrawing();
+            chart.EnableExtraDrawingMouseSafety();
 
             if (selection == AdvancedDrawingSelection.Fibonacci)
                 chart.ActivateFibonacciRetracement();
@@ -128,9 +130,11 @@ namespace Trade.It
 
         private void ChartDrawingTabChanged(object? sender, EventArgs e)
         {
-            GetActiveChart()?.CancelDrawing();
-            GetActiveChart()?.CancelAdvancedDrawing();
-            GetActiveChart()?.CancelExtraDrawing();
+            var chart = GetActiveChart();
+            chart?.CancelDrawing();
+            chart?.CancelAdvancedDrawing();
+            chart?.CancelExtraDrawing();
+            chart?.EnableExtraDrawingMouseSafety();
             ResetDrawingToolButtons();
         }
 
