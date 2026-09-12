0a8afd1eb6160354a5d8823a723682cbf250dd98
gfetmail-rgb <gfetmail@gmail.com>
2026-09-12 19:21:31 +0330

    Implement chart and drawing color settings

--- SettingsForm.cs
+++ SettingsForm.cs
@@ -8,6 +8,8 @@ namespace Trade.It
 
     public partial class SettingsForm : Form
     {
+        private readonly Dictionary<Button, string> drawingColorKeys = new();
+
         public ChartDisplayMode ChartDisplayMode
         {
             get => separateTabsRadioButton.Checked ? ChartDisplayMode.SeparateTabs : ChartDisplayMode.SingleTab;
@@ -31,7 +33,103 @@ namespace Trade.It
             RightToLeftLayout = true;
             ChartDisplayMode = currentMode;
             ChartRightEmptyPercent = chartRightEmptyPercent;
+            LoadColorButtons();
+            AttachColorEvents();
         }
+
+        private void LoadColorButtons()
+        {
+            SetColorButton(risingColorButton, ChartAppearanceSettings.RisingCandleColor);
+            SetColorButton(fallingColorButton, ChartAppearanceSettings.FallingCandleColor);
+            SetColorButton(lineColorButton, ChartAppearanceSettings.LineChartColor);
+
+            drawingColorKeys.Clear();
+            drawingColorKeys[trendLineColorButton] = nameof(ChartAppearanceSettings.TrendLineColor);
+            drawingColorKeys[trendChannelColorButton] = nameof(ChartAppearanceSettings.TrendChannelColor);
+            drawingColorKeys[horizontalDoubleArrowColorButton] = nameof(ChartAppearanceSettings.HorizontalDoubleArrowColor);
+            drawingColorKeys[verticalDoubleArrowColorButton] = nameof(ChartAppearanceSettings.VerticalDoubleArrowColor);
+            drawingColorKeys[horizontalRayColorButton] = nameof(ChartAppearanceSettings.HorizontalRayColor);
+            drawingColorKeys[trendLineWithArrowColorButton] = nameof(ChartAppearanceSettings.TrendLineWithArrowColor);
+            drawingColorKeys[rectangleColorButton] = nameof(ChartAppearanceSettings.RectangleColor);
+            drawingColorKeys[fibonacciRetracementColorButton] = nameof(ChartAppearanceSettings.FibonacciRetracementColor);
+            drawingColorKeys[textLabelColorButton] = nameof(ChartAppearanceSettings.TextLabelColor);
+            drawingColorKeys[pitchforkColorButton] = nameof(ChartAppearanceSettings.PitchforkColor);
+            drawingColorKeys[fibonacciExtensionColorButton] = nameof(ChartAppearanceSettings.FibonacciExtensionColor);
+            drawingColorKeys[measureColorButton] = nameof(ChartAppearanceSettings.MeasureColor);
+
+            foreach (var pair in drawingColorKeys)
+                SetColorButton(pair.Key, GetDrawingColor(pair.Value));
+        }
+
+        private void AttachColorEvents()
+        {
+            risingColorButton.Click += (_, _) => PickColor(risingColorButton, ChartAppearanceSettings.RisingCandleColor);
+            fallingColorButton.Click += (_, _) => PickColor(fallingColorButton, ChartAppearanceSettings.FallingCandleColor);
+            lineColorButton.Click += (_, _) => PickColor(lineColorButton, ChartAppearanceSettings.LineChartColor);
+
+            foreach (var pair in drawingColorKeys)
+                pair.Key.Click += (_, _) => PickDrawingColor(pair.Key, pair.Value);
+
+            resetChartColorsButton.Click += (_, _) =>
+            {
+                ChartAppearanceSettings.ResetChartColors();
+                LoadColorButtons();
+            };
+
+            resetDrawingColorsButton.Click += (_, _) =>
+            {
+                ChartAppearanceSettings.ResetDrawingColors();
+                LoadColorButtons();
+            };
+        }
+
+        private void PickColor(Button button, Color current)
+        {
+            using var dialog = new ColorDialog { Color = current, FullOpen = true };
+            if (dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                SetColorButton(button, dialog.Color);
+                if (button == risingColorButton)
+                    ChartAppearanceSettings.SetChartColors(dialog.Color, ChartAppearanceSettings.FallingCandleColor, ChartAppearanceSettings.LineChartColor);
+                else if (button == fallingColorButton)
+                    ChartAppearanceSettings.SetChartColors(ChartAppearanceSettings.RisingCandleColor, dialog.Color, ChartAppearanceSettings.LineChartColor);
+                else
+                    ChartAppearanceSettings.SetChartColors(ChartAppearanceSettings.RisingCandleColor, ChartAppearanceSettings.FallingCandleColor, dialog.Color);
+            }
+        }
+
+        private void PickDrawingColor(Button button, string key)
+        {
+            using var dialog = new ColorDialog { Color = GetDrawingColor(key), FullOpen = true };
+            if (dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                ChartAppearanceSettings.SetDrawingColor(key, dialog.Color);
+                SetColorButton(button, dialog.Color);
+            }
+        }
+
+        private static void SetColorButton(Button button, Color color)
+        {
+            button.BackColor = color;
+            button.ForeColor = color.GetBrightness() < 0.5f ? Color.White : Color.Black;
+        }
+
+        private static Color GetDrawingColor(string key) => key switch
+        {
+            nameof(ChartAppearanceSettings.TrendLineColor) => ChartAppearanceSettings.TrendLineColor,
+            nameof(ChartAppearanceSettings.TrendChannelColor) => ChartAppearanceSettings.TrendChannelColor,
+            nameof(ChartAppearanceSettings.HorizontalDoubleArrowColor) => ChartAppearanceSettings.HorizontalDoubleArrowColor,
+            nameof(ChartAppearanceSettings.VerticalDoubleArrowColor) => ChartAppearanceSettings.VerticalDoubleArrowColor,
+            nameof(ChartAppearanceSettings.HorizontalRayColor) => ChartAppearanceSettings.HorizontalRayColor,
+            nameof(ChartAppearanceSettings.TrendLineWithArrowColor) => ChartAppearanceSettings.TrendLineWithArrowColor,
+            nameof(ChartAppearanceSettings.RectangleColor) => ChartAppearanceSettings.RectangleColor,
+            nameof(ChartAppearanceSettings.FibonacciRetracementColor) => ChartAppearanceSettings.FibonacciRetracementColor,
+            nameof(ChartAppearanceSettings.TextLabelColor) => ChartAppearanceSettings.TextLabelColor,
+            nameof(ChartAppearanceSettings.PitchforkColor) => ChartAppearanceSettings.PitchforkColor,
+            nameof(ChartAppearanceSettings.FibonacciExtensionColor) => ChartAppearanceSettings.FibonacciExtensionColor,
+            nameof(ChartAppearanceSettings.MeasureColor) => ChartAppearanceSettings.MeasureColor,
+            _ => Color.Black
+        };
 
         private void okButton_Click(object? sender, EventArgs e)
         {
@@ -43,6 +141,8 @@ namespace Trade.It
                 return;
             }
 
+            ChartAppearanceSettings.SetChartRightEmptyPercent(ChartRightEmptyPercent);
+            ChartAppearanceSettings.Save();
             DialogResult = DialogResult.OK;
             Close();
         }
