e1bb70b947afc9aa785c56d21b56909bee92c365
gfetmail-rgb <gfetmail@gmail.com>
2026-09-14 23:42:55 +0330

    Fix chart tab close handler initialization

--- MainForm.ChartTabs.cs
+++ MainForm.ChartTabs.cs
@@ -5,13 +5,6 @@ namespace Trade.It
     public partial class MainForm
     {
         private bool chartTabCloseHandlerInitialized;
-        private readonly bool chartTabCloseHandlerHooked = RegisterChartTabCloseHandler();
-
-        private bool RegisterChartTabCloseHandler()
-        {
-            HandleCreated += MainForm_HandleCreatedForChartTabs;
-            return true;
-        }
 
         private void MainForm_HandleCreatedForChartTabs(object? sender, EventArgs e)
         {
