230752bf004537b0d37887f2deb27e8aac8c9275
gfetmail-rgb <gfetmail@gmail.com>
2026-09-19 20:34:03 +0330

    Rename horizontal and vertical drawing tools

--- TradingChartControl.Support.cs
+++ TradingChartControl.Support.cs
@@ -21,8 +21,8 @@ namespace Trade.It
         {
             ChartDrawingTool.TrendLine => ChartAppearanceSettings.TrendLineColor,
             ChartDrawingTool.TrendChannel => ChartAppearanceSettings.TrendChannelColor,
-            ChartDrawingTool.HorizontalDoubleArrow => ChartAppearanceSettings.HorizontalDoubleArrowColor,
-            ChartDrawingTool.VerticalDoubleArrow => ChartAppearanceSettings.VerticalDoubleArrowColor,
+            ChartDrawingTool.HorizontalLine => ChartAppearanceSettings.HorizontalLineColor,
+            ChartDrawingTool.VerticalLine => ChartAppearanceSettings.VerticalLineColor,
             ChartDrawingTool.HorizontalRay => ChartAppearanceSettings.HorizontalRayColor,
             ChartDrawingTool.TrendLineWithArrow => ChartAppearanceSettings.TrendLineWithArrowColor,
             ChartDrawingTool.Rectangle => ChartAppearanceSettings.RectangleColor,
