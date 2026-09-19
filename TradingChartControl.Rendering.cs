d9b3efdfa2e40cae3b1597a592fca9b22e6efdac
gfetmail-rgb <gfetmail@gmail.com>
2026-09-19 20:58:20 +0330

    Use current paint context for extra drawing rendering

--- TradingChartControl.Rendering.cs
+++ TradingChartControl.Rendering.cs
@@ -106,7 +106,7 @@ namespace Trade.It
 
             DrawDrawings(e.Graphics, plot, visible.Count, min, max);
             RenderAdvancedDrawings(e.Graphics);
-            RenderExtraDrawings(e.Graphics);
+            RenderExtraDrawings(e.Graphics, plot, visible.Count, min, max);
 
             if (drawingInProgress && activeDrawingTool != ChartDrawingTool.None && IsInsidePlot(drawingCurrentPoint))
             {
