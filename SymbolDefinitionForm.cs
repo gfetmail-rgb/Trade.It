023cfe5ece2ab66629ac27bf8422ba287aaf0229
gfetmail-rgb <gfetmail@gmail.com>
2026-09-23 20:17:54 +0330

    Refresh symbol grid when reloading asset categories

--- SymbolDefinitionForm.cs
+++ SymbolDefinitionForm.cs
@@ -352,6 +352,7 @@ public sealed partial class SymbolDefinitionForm : Form
         symbolTextBox.Clear();
         nameTextBox.Clear();
         SetComboDefaults();
+        LoadGrid();
         marketTreeView.SelectedNode = null;
         symbolTextBox.Focus();
     }
