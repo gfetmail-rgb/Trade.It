6b6784f22f9eb8bbb71c11f30ef2f54272f50165
Rasoul Moradi <ASEXBO@hotmail.com>
2026-09-23 19:36:43 +0330

    12345

--- SymbolDefinitionForm.Designer.cs
+++ SymbolDefinitionForm.Designer.cs
@@ -30,6 +30,15 @@ partial class SymbolDefinitionForm
     {
         components = new System.ComponentModel.Container();
         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SymbolDefinitionForm));
+        DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+        DataGridViewCellStyle dataGridViewCellStyle9 = new DataGridViewCellStyle();
+        DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+        DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+        DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+        DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
+        DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
+        DataGridViewCellStyle dataGridViewCellStyle7 = new DataGridViewCellStyle();
+        DataGridViewCellStyle dataGridViewCellStyle8 = new DataGridViewCellStyle();
         symbolTextBox = new TextBox();
         nameTextBox = new TextBox();
         marketTreeView = new TreeView();
@@ -74,28 +83,27 @@ partial class SymbolDefinitionForm
         // 
         // marketTreeView
         // 
-        marketTreeView.Location = new Point(860, 55);
-        marketTreeView.Name = "marketTreeView";
-        marketTreeView.Size = new Size(401, 360);
-        marketTreeView.TabIndex = 25;
         marketTreeView.HideSelection = false;
+        marketTreeView.Location = new Point(884, 6);
+        marketTreeView.Name = "marketTreeView";
         marketTreeView.RightToLeft = RightToLeft.Yes;
         marketTreeView.RightToLeftLayout = true;
-        // 
+        marketTreeView.Size = new Size(401, 682);
+        marketTreeView.TabIndex = 25;
         // 
         // assetComboBox
         // 
         assetComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
         assetComboBox.Items.AddRange(new object[] { "سهام نیست", "سهام", "صندوق ETF سهامی", "صندوق ETF کالایی" });
-        assetComboBox.Location = new Point(359, 76);
+        assetComboBox.Location = new Point(75, 76);
         assetComboBox.Name = "assetComboBox";
-        assetComboBox.Size = new Size(175, 30);
+        assetComboBox.Size = new Size(620, 30);
         assetComboBox.TabIndex = 5;
         // 
         // newButton
         // 
         newButton.AutoSize = true;
-        newButton.Location = new Point(701, 118);
+        newButton.Location = new Point(99, 134);
         newButton.Name = "newButton";
         newButton.Size = new Size(75, 35);
         newButton.TabIndex = 8;
@@ -105,7 +113,7 @@ partial class SymbolDefinitionForm
         // saveButton
         // 
         saveButton.AutoSize = true;
-        saveButton.Location = new Point(787, 118);
+        saveButton.Location = new Point(180, 134);
         saveButton.Name = "saveButton";
         saveButton.Size = new Size(75, 35);
         saveButton.TabIndex = 9;
@@ -115,7 +123,7 @@ partial class SymbolDefinitionForm
         // deleteButton
         // 
         deleteButton.AutoSize = true;
-        deleteButton.Location = new Point(994, 118);
+        deleteButton.Location = new Point(261, 134);
         deleteButton.Name = "deleteButton";
         deleteButton.Size = new Size(75, 35);
         deleteButton.TabIndex = 10;
@@ -125,7 +133,7 @@ partial class SymbolDefinitionForm
         // deleteAllButton
         // 
         deleteAllButton.AutoSize = true;
-        deleteAllButton.Location = new Point(1075, 118);
+        deleteAllButton.Location = new Point(342, 134);
         deleteAllButton.Name = "deleteAllButton";
         deleteAllButton.Size = new Size(100, 35);
         deleteAllButton.TabIndex = 11;
@@ -135,7 +143,7 @@ partial class SymbolDefinitionForm
         // importButton
         // 
         importButton.AutoSize = true;
-        importButton.Location = new Point(868, 118);
+        importButton.Location = new Point(448, 134);
         importButton.Name = "importButton";
         importButton.Size = new Size(120, 35);
         importButton.TabIndex = 12;
@@ -146,7 +154,7 @@ partial class SymbolDefinitionForm
         // exportButton
         // 
         exportButton.AutoSize = true;
-        exportButton.Location = new Point(280, 118);
+        exportButton.Location = new Point(574, 134);
         exportButton.Name = "exportButton";
         exportButton.Size = new Size(145, 35);
         exportButton.TabIndex = 13;
@@ -157,7 +165,7 @@ partial class SymbolDefinitionForm
         // closeButton
         // 
         closeButton.AutoSize = true;
-        closeButton.Location = new Point(1186, 118);
+        closeButton.Location = new Point(725, 134);
         closeButton.Name = "closeButton";
         closeButton.Size = new Size(75, 35);
         closeButton.TabIndex = 14;
@@ -167,7 +175,7 @@ partial class SymbolDefinitionForm
         // countLabel
         // 
         countLabel.AutoSize = true;
-        countLabel.Location = new Point(22, 131);
+        countLabel.Location = new Point(400, 182);
         countLabel.Name = "countLabel";
         countLabel.Size = new Size(68, 22);
         countLabel.TabIndex = 15;
@@ -180,85 +188,106 @@ partial class SymbolDefinitionForm
         symbolsDataGridView.AllowUserToResizeRows = false;
         symbolsDataGridView.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
         symbolsDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleRight;
+        dataGridViewCellStyle1.BackColor = SystemColors.Control;
+        dataGridViewCellStyle1.Font = new Font("Tahoma", 9F);
+        dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+        dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+        dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+        dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+        symbolsDataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
         symbolsDataGridView.ColumnHeadersHeight = 34;
         symbolsDataGridView.Columns.AddRange(new DataGridViewColumn[] { rowNumberColumn, symbolTitleColumn, nameColumn, exchangeColumn, marketColumn, boardColumn, assetColumn });
-        symbolsDataGridView.Location = new Point(10, 165);
+        dataGridViewCellStyle9.Alignment = DataGridViewContentAlignment.MiddleRight;
+        dataGridViewCellStyle9.BackColor = SystemColors.Window;
+        dataGridViewCellStyle9.Font = new Font("Tahoma", 9F);
+        dataGridViewCellStyle9.ForeColor = SystemColors.ControlText;
+        dataGridViewCellStyle9.SelectionBackColor = SystemColors.Highlight;
+        dataGridViewCellStyle9.SelectionForeColor = SystemColors.HighlightText;
+        dataGridViewCellStyle9.WrapMode = DataGridViewTriState.False;
+        symbolsDataGridView.DefaultCellStyle = dataGridViewCellStyle9;
+        symbolsDataGridView.Location = new Point(10, 212);
         symbolsDataGridView.MultiSelect = false;
         symbolsDataGridView.Name = "symbolsDataGridView";
         symbolsDataGridView.ReadOnly = true;
         symbolsDataGridView.RightToLeft = RightToLeft.Yes;
-        symbolsDataGridView.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
-        symbolsDataGridView.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
         symbolsDataGridView.RowHeadersVisible = false;
         symbolsDataGridView.RowHeadersWidth = 62;
         symbolsDataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-        symbolsDataGridView.Size = new Size(1279, 520);
+        symbolsDataGridView.Size = new Size(868, 473);
         symbolsDataGridView.TabIndex = 0;
         // 
         // rowNumberColumn
         // 
+        dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleRight;
+        rowNumberColumn.DefaultCellStyle = dataGridViewCellStyle2;
         rowNumberColumn.FillWeight = 45F;
         rowNumberColumn.HeaderText = "ردیف";
         rowNumberColumn.MinimumWidth = 45;
         rowNumberColumn.Name = "rowNumberColumn";
         rowNumberColumn.ReadOnly = true;
-        rowNumberColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
         rowNumberColumn.SortMode = DataGridViewColumnSortMode.NotSortable;
         // 
         // symbolTitleColumn
         // 
+        dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleRight;
+        symbolTitleColumn.DefaultCellStyle = dataGridViewCellStyle3;
         symbolTitleColumn.HeaderText = "نماد";
         symbolTitleColumn.MinimumWidth = 8;
         symbolTitleColumn.Name = "symbolTitleColumn";
         symbolTitleColumn.ReadOnly = true;
-        symbolTitleColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
         symbolTitleColumn.SortMode = DataGridViewColumnSortMode.NotSortable;
         // 
         // nameColumn
         // 
+        dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleRight;
+        nameColumn.DefaultCellStyle = dataGridViewCellStyle4;
         nameColumn.FillWeight = 150F;
         nameColumn.HeaderText = "نام";
         nameColumn.MinimumWidth = 8;
         nameColumn.Name = "nameColumn";
         nameColumn.ReadOnly = true;
-        nameColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
         nameColumn.SortMode = DataGridViewColumnSortMode.NotSortable;
         // 
         // exchangeColumn
         // 
+        dataGridViewCellStyle5.Alignment = DataGridViewContentAlignment.MiddleRight;
+        exchangeColumn.DefaultCellStyle = dataGridViewCellStyle5;
         exchangeColumn.HeaderText = "بورس";
         exchangeColumn.MinimumWidth = 8;
         exchangeColumn.Name = "exchangeColumn";
         exchangeColumn.ReadOnly = true;
-        exchangeColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
         exchangeColumn.SortMode = DataGridViewColumnSortMode.NotSortable;
         // 
         // marketColumn
         // 
+        dataGridViewCellStyle6.Alignment = DataGridViewContentAlignment.MiddleRight;
+        marketColumn.DefaultCellStyle = dataGridViewCellStyle6;
         marketColumn.HeaderText = "بازار";
         marketColumn.MinimumWidth = 8;
         marketColumn.Name = "marketColumn";
         marketColumn.ReadOnly = true;
-        marketColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
         marketColumn.SortMode = DataGridViewColumnSortMode.NotSortable;
         // 
         // boardColumn
         // 
+        dataGridViewCellStyle7.Alignment = DataGridViewContentAlignment.MiddleRight;
+        boardColumn.DefaultCellStyle = dataGridViewCellStyle7;
         boardColumn.HeaderText = "تابلو";
         boardColumn.MinimumWidth = 8;
         boardColumn.Name = "boardColumn";
         boardColumn.ReadOnly = true;
-        boardColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
         boardColumn.SortMode = DataGridViewColumnSortMode.NotSortable;
         // 
         // assetColumn
         // 
+        dataGridViewCellStyle8.Alignment = DataGridViewContentAlignment.MiddleRight;
+        assetColumn.DefaultCellStyle = dataGridViewCellStyle8;
         assetColumn.FillWeight = 80F;
         assetColumn.HeaderText = "دارایی";
         assetColumn.MinimumWidth = 8;
         assetColumn.Name = "assetColumn";
         assetColumn.ReadOnly = true;
-        assetColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
         assetColumn.SortMode = DataGridViewColumnSortMode.NotSortable;
         // 
         // label00
@@ -281,7 +310,7 @@ partial class SymbolDefinitionForm
         // 
         // label30
         // 
-        label30.Location = new Point(860, 20);
+        label30.Location = new Point(778, 19);
         label30.Name = "label30";
         label30.Size = new Size(100, 30);
         label30.TabIndex = 19;
@@ -290,9 +319,9 @@ partial class SymbolDefinitionForm
         // 
         // label11
         // 
-        label11.Location = new Point(262, 76);
+        label11.Location = new Point(12, 75);
         label11.Name = "label11";
-        label11.Size = new Size(90, 30);
+        label11.Size = new Size(57, 30);
         label11.TabIndex = 21;
         label11.Text = "دارایی";
         label11.TextAlign = ContentAlignment.MiddleRight;
--- SymbolDefinitionForm.GridDisplay.cs
+++ SymbolDefinitionForm.GridDisplay.cs
@@ -13,12 +13,7 @@ public sealed partial class SymbolDefinitionForm
         if (e.RowIndex < 0 || e.RowIndex >= symbolsDataGridView.Rows.Count)
             return;
 
-        if (symbolsDataGridView.Rows[e.RowIndex].Tag is not SymbolDefinition item)
+        if (symbolsDataGridView.Rows[e.RowIndex].Tag is not SymbolDefinition)
             return;
-
-        if (e.ColumnIndex == symbolsDataGridView.Columns["fundTypeColumn"].Index)
-            e.Value = string.IsNullOrWhiteSpace(item.FundType) ? SymbolDefinitionRules.EmptyOption : item.FundType;
-        else if (e.ColumnIndex == symbolsDataGridView.Columns["industryGroupColumn"].Index)
-            e.Value = string.IsNullOrWhiteSpace(item.IndustryGroup) ? SymbolDefinitionRules.EmptyOption : item.IndustryGroup;
     }
 }
